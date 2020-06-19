3082e6a5b9d8e50d5b62aeebdc61f22d6a9d251f
RachelMcCloud <rachelmccloud22@gmail.com>
2020-06-18 21:39:07 -0400

    Second deliverable two

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Deliverable_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string input = "";
+            string message = "";
+            int checksum = 0;
+
+            Console.WriteLine("What is your message?");
+
+            input = Console.ReadLine().ToUpper();
+
+            //for (int i = 0; i < input.Length; i++)
+            //{
+            //    char letter = input[i];
+            //}
+
+
+            foreach(char letter in input)
+            {
+                int letterValue = (int)letter;
+                checksum += letterValue;
+
+                int alphabetNumber = letterValue - 65 + 1;
+                message = message + alphabetNumber + "-";
+
+            }
+
+            Console.WriteLine("Your encoded message is " + message);
+            Console.WriteLine("Message checksum is " + checksum);
+            
+
+        }
+    }
+}
